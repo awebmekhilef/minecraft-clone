d03dad69b1f9b222a46e4bec5987270950ab8423
Aweb Mekhilef <awebmekhilef@gmail.com>
2022-03-20 10:34:00 +1100

    Added falloff maps

--- Assets/_Scripts/TerrainDisplay.cs
+++ Assets/_Scripts/TerrainDisplay.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 public class TerrainDisplay : MonoBehaviour
 {
 	[Header("Settings")]
-	[SerializeField] int _width;
-	[SerializeField] int _height;
+	[SerializeField] int _size;
 	[SerializeField] int _seed;
 	[SerializeField] int _heightMultiplier;
 	[SerializeField] int _scale;
 	[SerializeField] int _octaves;
 	[SerializeField] float _persistence;
 	[SerializeField] float _lacunarity;
-	[SerializeField] Gradient _gradient;
+	[SerializeField] Gradient _colorGradient;
+	[SerializeField] Gradient _fallOffGradient;
 	[SerializeField] AnimationCurve _heightCurve;
 
 	MeshFilter _meshFilter;
@@ -25,8 +25,21 @@ public class TerrainDisplay : MonoBehaviour
 
 	public void GenerateTerrain()
 	{
-		float[,] noiseMap = Noise.Generate(_width, _height, _seed, _scale, _octaves, _persistence, _lacunarity);
+		float[,] noiseMap = Noise.Generate(_size, _size, _seed, _scale, _octaves, _persistence, _lacunarity);
+
+		for (int i = 0; i < _size; i++)
+		{
+			for (int j = 0; j < _size; j++)
+			{
+				float x = (float)i / _size * 2 - 1;
+				float y = (float)j / _size * 2 - 1;
+
+				float max = Mathf.Max(Mathf.Abs(x), Mathf.Abs(y));
+				noiseMap[i, j] = noiseMap[i, j] * _fallOffGradient.Evaluate(max).a;
+			}
+		}
+
 		_meshFilter.mesh = MeshGenerator.GenerateMesh(noiseMap, _heightMultiplier, _heightCurve);
-		_meshRenderer.sharedMaterial.mainTexture = TextureGenerator.GenerateColoredTexture(noiseMap, _gradient);
+		_meshRenderer.sharedMaterial.mainTexture = TextureGenerator.GenerateColoredTexture(noiseMap, _colorGradient);
 	}
 }
