a66d392624c3d931eddad6a3c9c878e8df5d06b0
Aweb Mekhilef <awebmekhilef@gmail.com>
2022-03-19 16:12:31 +1100

    Noise octaves

--- Assets/_Scripts/MapDisplay.cs
+++ Assets/_Scripts/MapDisplay.cs
@@ -6,6 +6,9 @@ public class MapDisplay : MonoBehaviour
 	[SerializeField] int _width;
 	[SerializeField] int _height;
 	[SerializeField] int _scale;
+	[SerializeField] int _octaves;
+	[SerializeField] float _persistence;
+	[SerializeField] float _lacunarity;
 	[SerializeField] Gradient _gradient;
 
 	MeshFilter _meshFilter;
@@ -16,7 +19,7 @@ public class MapDisplay : MonoBehaviour
 		_meshFilter = GetComponent<MeshFilter>();
 		_meshRenderer = GetComponent<MeshRenderer>();
 
-		float[,] noiseMap = Noise.Generate(_width, _height, _scale);
+		float[,] noiseMap = Noise.Generate(_width, _height, _scale, _octaves, _persistence, _lacunarity);
 		_meshFilter.mesh = MeshGenerator.GenerateMesh(noiseMap);
 		_meshRenderer.material.mainTexture = TextureGenerator.GenerateColoredTexture(noiseMap, _gradient);
 	}
--- Assets/_Scripts/Noise.cs
+++ Assets/_Scripts/Noise.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public static class Noise
 {
-	public static float[,] Generate(int width, int height, float scale)
+	public static float[,] Generate(int width, int height, float scale, int octaves, float persistence, float lacunarity)
 	{
 		float[,] noiseMap = new float[width, height];
+		int seed = Random.Range(0, 10000);
+
+		float minValue = float.MaxValue;
+		float maxValue = float.MinValue;
 
 		for (int y = 0; y < height; y++)
 		{
 			for (int x = 0; x < width; x++)
 			{
-				noiseMap[x, y] = Mathf.PerlinNoise(x / scale, y / scale);
+				float amplitude = 1f;
+				float frequency = 1f;
+				float noiseValue = 0f;
+
+				for (int i = 0; i < octaves; i++)
+				{
+					noiseValue += amplitude * Mathf.PerlinNoise(
+						((seed + x) / scale) * frequency,
+						((seed + y) / scale) * frequency);
+
+					amplitude *= persistence;
+					frequency *= lacunarity;
+				}
+
+				noiseMap[x, y] = noiseValue;
+
+				if (noiseValue > maxValue)
+					maxValue = noiseValue;
+				if (noiseValue < minValue)
+					minValue = noiseValue;
+			}
+		}
+
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				noiseMap[x, y] = Mathf.InverseLerp(minValue, maxValue, noiseMap[x, y]);
 			}
 		}
 
